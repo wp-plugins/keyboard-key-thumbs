f488288b848c26e2770726d63becaceaea6b2b4e
C-Realm <C-Realm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-05 17:16:14 +0000

    Adding first version of ketboard-key-thumbs
    git-svn-id: https://plugins.svn.wordpress.org/keyboard-key-thumbs/trunk@459690 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ keyboard-key-thumbs.php
@@ -0,0 +1,72 @@
+<?php 
+/*
+Plugin Name: Keyboard Key Thumbs
+Description: This plugin is intended to help those that need to write an article that contains keyboard key combinations. It simply replace the shortcode [key] with the tag <kdb> and display a key image.
+Version: 0.1
+Author: Shadow silver
+Plugin URI: http://crealm.altervista.org/mainblog/keyboard-key-thumbs/
+Author URI: http://crealm.altervista.org/mainblog
+License: LGPL3
+*/
+
+class Keyboard_Key_Thumbs
+{
+    function __construct()
+    {
+        add_shortcode('key', array( &$this, 'kbd_substitution'));
+        //add_shortcode('galley', array( &$this, 'kbd_substitution'));
+        add_action('wp_head', array( &$this, 'kbd_css'),10,1);
+    }
+    
+    function kbd_css($content)
+    {
+        echo '<link rel="stylesheet" type="text/css" href="'. plugins_url() . "/" . dirname(plugin_basename(__FILE__)) . '/kdb-shortcode.css" />'; 
+        //return  $link;
+    }
+
+    function kdb_add_tag($value)
+    {
+        $class = "kdb-shortcode";
+        if (preg_match("/shift/i",$value) > 0)
+        { $class .= " shift"; $value=ucfirst($value);}
+        else if (preg_match("/ctrl|control/i",$value) > 0)
+        { $class .= " control"; $value=ucfirst($value);}
+        else if (preg_match("/alt/i",$value) > 0)
+        { $class .= " alt"; $value=ucfirst($value);}
+        else if ( strlen($value) == 1)
+        { $class .= " char";}
+        else if ( strlen($value) > 1 && strlen($value) < 4)
+        { $class .= " middle";}
+        else if ( strlen($value) > 3 && strlen($value) < 6)
+        { $class .= " large";}
+        
+        return "<kdb class=\"$class\">" . $value . "</kdb>";
+    }
+
+    function kbd_substitution( $atts, $content=null)
+    {
+        if ($content == null)
+            return "";
+
+        $content = preg_replace("/(\w+) \+ (\w+)/","$1+$2",$content);
+        $key_expressions = preg_split("/ /",$content);
+        $return ="";
+
+        foreach ($key_expressions as $key_expression)
+        {
+            $key_combinations = preg_split("/\+/",$key_expression);
+            $key_combinations = array_map( array( &$this,"kdb_add_tag"),$key_combinations);
+
+            $return .= "<span class=\"kdb-shortcode\">" . implode("+",$key_combinations) . "</span> ";
+        }
+
+        return $return;
+        
+    }
+
+}
+
+
+$kdb_shortcode = new  Keyboard_Key_Thumbs();
+
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+===  Keyboard Key Thumbs ===
+Contributors: C-Realm
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=QKGQ9KKTD5EKA&lc=GB&item_name=C%2dRealm&item_number=000001&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Tags: keyboard, kdb, shortcode
+Tested up to: 3.2.1
+Stable tag: 4.3
+
+It simply replace the shortcode [key] with the tag kbd and images
+
+== Description ==
+
+This plugin is intended to help those that need to write an article that contains keyboard key combinations. It simply replace the shortcode &#91;key&#93; with the tag &lt;kdb&gt; and display a key image.
+
+== Installation ==
+
+
+1. Unzip the file
+2. Copy the plugin directory in your wordpress plugin.
+3. Insert into the shortcode &#91;key&#93;&#91;/key&#93; the key combinations separate by "+" that you would display (e.g. &#91;key&#93;Alt+c&#91;/key&#93;
+
+
+== Frequently Asked Questions ==
+ 
+
+== Screenshots ==
+
+1. An example output
+
+
+== Changelog ==
+
+
+= 0.1 =
+This version support all the char key and the shift,alt,ctrl key
+
